86001c49ce2b0d3b0e65ad71db2fbcd1290c6d61
ZhengxuYu <yuzxfred@gmail.com>
2026-04-07 22:48:11 +0100

    feat: add Vercel Analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.tsx'
+
+inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
